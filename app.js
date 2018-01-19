30d8dc306f76932c2e1c99b752ea9ebcaa0b3567
Gaetano Venezia III <grrtano@gmail.com>
2018-01-19 05:13:08 +0000

    API example call added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
-var request
+var request = require("request");
+
+// Example API call
+app.get("/results", function(req, res){
+   request("https://www.omdbapi.com/?apikey=thewdb&s=nexus", function(error, response, body){
+        if (!error && response.statusCode === 200){
+            res.send(body);
+        }
+   }); 
+});
 
 
 // Check that the server is running successfully
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server is running");
+   console.log("The movie search app is running");
 });
